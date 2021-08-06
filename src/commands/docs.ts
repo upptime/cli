3afab406993602fca4e516267fbc2af2de6e8a63
Nirmitjatana <nirmitjatana38@gmail.com>
2021-08-06 16:14:35 +0530

    feat: docs command working

--- src/commands/docs.ts
+++ src/commands/docs.ts
@@ -1,22 +1,10 @@
-// import {Command, flags} from '@oclif/command'
 import {Command} from '@oclif/command'
-// import global from 'browser'
-
+import cli from  'cli-ux'
 export default class Docs extends Command {
   static description = 'redirects to Upptime docs'
 
-  // static flags = {
-  //   help: flags.help({char: 'h'}),
-  //   // flag with a value (-n, --name=VALUE)
-  //   name: flags.string({char: 'n', description: 'name to print'}),
-  //   // flag with no value (-f, --force)
-  //   force: flags.boolean({char: 'f'}),
-  // }
   async run() {
     this.log('redirecting to https://upptime.js.org/')
-    // this.ref('redirecting to https://upptime.js.org/')
-    // window
-    window.open('https://upptime.js.org/', '_blank')
-    // run(['start https://upptime.js.org/'])
+    cli.open('https://upptime.js.org/')
   }
 }
