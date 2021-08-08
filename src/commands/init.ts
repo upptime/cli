70d3dbac18c7bcfbbe93563b623e025198ed3f96
Sakshi <choudharysakshi023@gmail.com>
2021-08-08 14:34:17 +0530

    feat(init):add yaml file on init

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,9 +1,11 @@
 import {Command} from '@oclif/command'
+const fs = require('fs')
 
 export default class Init extends Command {
   static description = 'initializes upptime'
 
   async run() {
+    fs.writeFileSync('uclirc.yaml', 'owner: upptime')
     this.log('successfully initialized!')
   }
 }
