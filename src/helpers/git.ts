a5700dd6df90b8a333d96474bb68fcbab14b375e
Jayant Katia <jayantkatia65@gmail.com>
2021-08-31 18:52:45 +0530

    feat(helper/git): change upptime's name, email scope to local and revert back to prev

--- src/helpers/git.ts
+++ src/helpers/git.ts
@@ -1,19 +1,39 @@
 import {exec} from 'shelljs'
 
-export const commit = (
-  message: string,
+const _getNameAndEmail = () => {
+  const currName = exec('git config --list', {silent: true}).grep('user.name=').stdout.split(
+    'user.name='
+  ).pop()?.trim()
+  const currEmail = exec('git config --list', {silent: true}).grep('user.email=').stdout.split(
+    'user.email='
+  ).pop()?.trim()
+  return {currName, currEmail}
+}
+
+const _setNameAndEmail = (
   name = 'Upptime Bot',
   email = '73812536+upptime-bot@users.noreply.github.com'
 ) => {
-  exec(`git config --global user.email "${email}"`)
-  exec(`git config --global user.name "${name}"`)
+  exec(`git config user.email "${email}"`)
+  exec(`git config user.name "${name}"`)
+}
+
+export const commit = (
+  message: string,
+) => {
+  const {currName: prevName, currEmail: prevEmail} = _getNameAndEmail()
+  _setNameAndEmail()
   exec('git add .')
   exec(`git commit -m "${message.replace(/"/g, "''")}"`)
+  _setNameAndEmail(prevName, prevEmail)
 }
 
 export const push = () => {
+  const {currName: prevName, currEmail: prevEmail} = _getNameAndEmail()
+  _setNameAndEmail()
   const result = exec('git push')
   if (result.includes('error:')) throw new Error(result)
+  _setNameAndEmail(prevName, prevEmail)
 }
 
 export const lastCommit = () => {
