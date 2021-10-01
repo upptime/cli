0dbe13920d3984593ff9b6b86c3bba2698ea9b02
Jayant Katia <jayantkatia65@gmail.com>
2021-10-02 02:04:53 +0530

    fix: init cmd when not a git repository

--- src/helpers/log-color.ts
+++ src/helpers/log-color.ts
@@ -1,14 +1,8 @@
-import {stat} from 'fs-extra'
 import {getConfig} from './config'
 
 export let isLogColor: undefined | boolean
 export const getIsLogColor = async () => {
   if (isLogColor) return isLogColor
-  try {
-    await stat('.uclirc.yml')
-  } catch (_) {
-    return isLogColor
-  }
   const config = await getConfig()
   isLogColor = config.logs?.colors
   return isLogColor
