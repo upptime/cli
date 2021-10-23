d913323b28cce478b310cdbdb7a0cb3258839b2f
Jayant Katia <jayantkatia65@gmail.com>
2021-10-23 21:43:30 +0530

    fix(cmd/config): check whether repo is upptime initialized

--- src/commands/config.ts
+++ src/commands/config.ts
@@ -1,4 +1,5 @@
 import Command from '../base'
+import {shouldContinue} from '../helpers/should-continue'
 import chalk from 'chalk'
 import child_process from 'child_process'
 import {flags} from '@oclif/command'
@@ -13,44 +14,61 @@ export default class Config extends Command {
 
   static flags = {
     help: flags.help({char: 'h', description: 'Show help for config cmd'}),
-    // 'add-site': flags.string({char: 's', description: 'Add endpoint to monitor'}),
-    // 'add-assignee': flags.boolean({char: 'q', description: 'Quiet'}),
+    'add-endpoint': flags.string({char: 'e', description: 'Add endpoint to monitor'}),
+    'add-notification': flags.boolean({char: 'n', description: 'Quiet'}),
   }
 
   async run() {
-    // const {flags} = this.parse(Config)
+    const {flags} = this.parse(Config)
 
-    const responses: any = await inquirer.prompt([{
-      name: 'action',
-      message: 'select to config',
-      type: 'list',
-      choices: [{
-        name: 'Add Notification provider', value: configOptions.ADD_ENDPOINT,
-      }, {
-        name: 'Add endpoint to monitor', value: configOptions.ADD_NOTIFICATION,
-      }, {
-        name: 'Other configurations (Open in editor)', value: configOptions.OPEN_EDITOR,
-      }],
-    }])
-    const response = responses.action
+    const shouldContinueObj = await shouldContinue()
+    if (!shouldContinueObj.shouldContinue) {
+      this.log(shouldContinueObj.message)
+      return
+    }
+
+    let response: any
+
+    if (Object.keys(flags).length === 0) {
+      const responses: any = await inquirer.prompt([{
+        name: 'action',
+        message: 'select to config',
+        type: 'list',
+        choices: [{
+          name: 'Add Notification provider', value: configOptions.ADD_NOTIFICATION,
+        }, {
+          name: 'Add endpoint to monitor', value: configOptions.ADD_ENDPOINT,
+        }, {
+          name: 'Other configurations (Open in editor)', value: configOptions.OPEN_EDITOR,
+        }],
+      }])
+      response = responses.action
+    } else if (flags['add-endpoint']) {
+      response  = configOptions.ADD_ENDPOINT
+    } else if (flags['add-notification']) {
+      response  = configOptions.ADD_NOTIFICATION
+    }
 
     switch (response) {
     case configOptions.ADD_ENDPOINT:
       // Code to add a website
+
       break
     case configOptions.ADD_NOTIFICATION:
       // Ask which notification provider
+
       break
     case configOptions.OPEN_EDITOR:
-      // Sakshi's spawn editor code, but make it platform aware
       this.spawnEditor()
       break
     }
   }
 
+  // platform aware
   getPlatformDefaultEditor() {
     if (process.platform === 'win32')
       return 'notepad'
+    // add more platforms
     return 'vi'
   }
 
---
+++ src/helpers/should-continue.ts
@@ -0,0 +1,20 @@
+import chalk from 'chalk'
+import {existsSync} from 'fs-extra'
+
+export class ShouldContinue {
+  message: string | undefined
+
+  shouldContinue: boolean
+
+  constructor(shouldContinue: boolean, message?: string) {
+    this.shouldContinue = shouldContinue
+    this.message = message
+  }
+}
+
+export async function shouldContinue() {
+  if (!existsSync('.uclirc.yml')) {
+    return new ShouldContinue(false, chalk.red('❌ Repository is not Upptime Initialized') + '\n' + chalk.blue('Try ') + chalk.yellow('upp run'))
+  }
+  return new ShouldContinue(true)
+}
